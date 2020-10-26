328787cd16a0b6c375fed7e6963c0b746814cfbe
junhong <climax1001@gmail.com>
2020-10-26 18:23:57 +0900

    201026 준홍 수정(시간표 정보)

--- app/src/main/java/com/example/capstone_ui_1/HomeFragment.java
+++ app/src/main/java/com/example/capstone_ui_1/HomeFragment.java
@@ -1,34 +1,148 @@
 package com.example.capstone_ui_1;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 
-public class HomeFragment extends Fragment  {
-    private ViewGroup viewGroup;
+import com.github.tlaabs.timetableview.Schedule;
+import com.github.tlaabs.timetableview.Time;
+import com.github.tlaabs.timetableview.TimetableView;
+import com.example.capstone_ui_1.Service.EditActivity;
+import com.example.capstone_ui_1.R;
+
+import java.util.ArrayList;
+
+import static android.view.ViewGroup.*;
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
     private Context context;
-    private MainActivity mainActivity;
-    private GridLayoutManager gridLayoutManager;
+    public static final int REQUEST_ADD = 1;
+    public static final int REQUEST_EDIT = 2;
+
+    private Button addBtn;
+    private Button clearBtn;
+    private Button saveBtn;
+    private Button loadBtn;
+
+    private  TimetableView timetable;
 
     @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        this.context = context;
-        mainActivity =(MainActivity)getContext();
+    public void onStart() {
+        super.onStart();
 
     }
-    @Nullable
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.home_fragment, container, false);
+        View view = (ViewGroup) inflater.inflate(R.layout.home_fragment, container, false);
 
-        return viewGroup;
+
+        init(view);
+        return view;
+    }
+
+
+
+    private void init(View v){
+        addBtn = (Button) v.findViewById(R.id.add_btn);
+        clearBtn = (Button) v.findViewById(R.id.clear_btn);
+        saveBtn = (Button) v.findViewById(R.id.save_btn);
+        loadBtn = (Button) v.findViewById(R.id.load_btn);
+
+        timetable = (TimetableView) v.findViewById(R.id.timetable);
+//        timetable.setHeaderHighlight(2);
+        initView();
+    }
+
+    private void initView(){
+        addBtn.setOnClickListener(this);
+        clearBtn.setOnClickListener(this);
+        saveBtn.setOnClickListener(this);
+        loadBtn.setOnClickListener(this);
+
+        timetable.setOnStickerSelectEventListener((idx, schedules) -> {
+            Intent i = new Intent(getView().getContext(), EditActivity.class);
+            i.putExtra("mode",REQUEST_EDIT);
+            i.putExtra("idx", idx);
+            i.putExtra("schedules", schedules);
+            startActivityForResult(i,REQUEST_EDIT);
+        });
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.add_btn:
+                Intent i = new Intent(getActivity(),EditActivity.class);
+                i.putExtra("mode",REQUEST_ADD);
+                startActivityForResult(i,REQUEST_ADD);
+                break;
+            case R.id.clear_btn:
+                timetable.removeAll();
+                break;
+            case R.id.save_btn:
+                saveByPreference(timetable.createSaveData());
+                break;
+            case R.id.load_btn:
+                loadSavedData();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case REQUEST_ADD:
+                if(resultCode == EditActivity.RESULT_OK_ADD){
+                    ArrayList<Schedule> item = (ArrayList<Schedule>)data.getSerializableExtra("schedules");
+                    timetable.add(item);
+                }
+                break;
+            case REQUEST_EDIT:
+                /** Edit -> Submit */
+                if(resultCode == EditActivity.RESULT_OK_EDIT){
+                    int idx = data.getIntExtra("idx",-1);
+                    ArrayList<Schedule> item = (ArrayList<Schedule>)data.getSerializableExtra("schedules");
+                    timetable.edit(idx,item);
+                }
+                /** Edit -> Delete */
+                else if(resultCode == EditActivity.RESULT_OK_DELETE){
+                    int idx = data.getIntExtra("idx",-1);
+                    timetable.remove(idx);
+                }
+                break;
+        }
+    }
+
+    /** save timetableView's data to SharedPreferences in json format */
+    private void saveByPreference(String data){
+        SharedPreferences mPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences.Editor editor = mPref.edit();
+        editor.putString("timetable_demo",data);
+        editor.commit();
+        Toast.makeText(getActivity(),"saved!",Toast.LENGTH_SHORT).show();
+    }
+
+    /** get json data from SharedPreferences and then restore the timetable */
+    private void loadSavedData(){
+        timetable.removeAll();
+        SharedPreferences mPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String savedData = mPref.getString("timetable_demo","");
+        if(savedData == null && savedData.equals("")) return;
+        timetable.load(savedData);
+        Toast.makeText(getActivity(),"loaded!",Toast.LENGTH_SHORT).show();
     }
 }
